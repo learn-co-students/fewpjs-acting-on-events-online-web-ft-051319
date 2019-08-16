6a44f8cce19945f2c8015fd5a13fd83e24e42b2c
wkpescherine <wkpescherine@gmail.com>
2019-08-16 10:57:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+var dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function(e) {
+    console.log(e.key);
+  });
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    var rightNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(rightNumbers, 10);
+   
+    if (left < 350) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
